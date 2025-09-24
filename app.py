fe3199495a4771c69df4b0814476aa12e15873b7
gunjan <gunjankumari12082004@gmail.com>
2025-09-24 13:42:10 +0530

    added

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
-from dotenv import load_dotenv 
-load_dotenv() 
-import streamlit as st 
-import os 
-import google.generativeai as genai 
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import google.generativeai as genai
 
+# Load .env file
+load_dotenv()
+
+# Configure Google API
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash") 
-# model = genai.GenerativeModel("gemini-pro") 
-# model = genai.GenerativeModel("gemini-1.5-pro") 
+# Choose Gemini model
+model = genai.GenerativeModel("gemini-1.5-flash")
+# model = genai.GenerativeModel("gemini-1.5-pro")
 
 def my_output(query):
-    response = model.generate_content(query) 
-    return response.text 
+    response = model.generate_content(query)
+    return response.text
 
-# UI Development using streamlit 
-
+# Streamlit UI
 st.set_page_config(page_title="Mattie_bot")
-st.header("Mattie_bot") 
-input = st.text_input("Input " , key = "input")  
-submit = st.button("Ask your query") 
+st.header("Mattie_bot")
 
-if submit :
-    response = my_output(input) 
-    st.subheader("The Response is=")
+input_text = st.text_input("Input", key="input")
+submit = st.button("Ask your query")
+
+if submit:
+    response = my_output(input_text)
+    st.subheader("The Response is =")
     st.write(response)
